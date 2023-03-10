9bea36bbaafed3e92aca0e6244196979bf536507
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-03-10 16:09:20 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap';
+import './index.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar sticky-top bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand">Clicky Game</a>
+      </div>
+    </nav>
+
+    <div class="card" style="width: 18rem;">
+  <img src="" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+  </div>
+</div>
     </div>
   );
 }
